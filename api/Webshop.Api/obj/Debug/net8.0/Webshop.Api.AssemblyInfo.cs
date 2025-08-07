8e0f634fddc818dae0c3ec977fba5cc5682da464
Alex <Alexanderstubmichelsen@gmail.com>
2025-08-07 10:00:12 +0200

    5000 for backend

--- api/Webshop.Api/obj/Debug/net8.0/Webshop.Api.AssemblyInfo.cs
+++ api/Webshop.Api/obj/Debug/net8.0/Webshop.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webshop.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c256b671dc80638c5d9793c3d436d8b2ead469e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237419c32424578a0894bccef507737eae470b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webshop.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webshop.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
