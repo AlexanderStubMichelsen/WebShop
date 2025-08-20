ef14d156f0c8a80d88ac08c87aad969a43813121
Alex <Alexanderstubmichelsen@gmail.com>
2025-08-20 11:01:52 +0200

    config export in next.config.ts and out
    folder is created by npm build.

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,19 +3,11 @@ import type { NextConfig } from "next";
 const isExport = process.env.NEXT_OUTPUT === "export";
 
 const nextConfig: NextConfig = {
-  ...(isExport
-    ? {
-        output: "export",
-        images: {
-          unoptimized: true,
-          domains: ["via.placeholder.com", "picsum.photos"],
-        },
-      }
-    : {
-        images: {
-          domains: ["via.placeholder.com", "picsum.photos"],
-        },
-      }),
+  output: "export",
+  images: {
+    domains: ["via.placeholder.com", "picsum.photos"],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
